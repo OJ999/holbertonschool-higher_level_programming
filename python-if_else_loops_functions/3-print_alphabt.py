10de7275e332a1c36268770ed1f986e2ce07ae3f
Omar J <omarj819@gmail.com>
2024-01-23 18:07:26 +0200

    A

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 for char in range(ord('a'), ord('z') + 1):
-    if char != ord('e') and char != ord('q'):
-        print("{}".format(chr(char)), end="")
+    if chr(char) != 'e' and chr(char) != 'q':
+        print("{:c}".format(char), end="")
+
 print()
