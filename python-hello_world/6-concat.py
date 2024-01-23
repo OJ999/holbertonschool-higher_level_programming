28f9ff2b509c23e30ae15e06335f0d34ad04aebf
Omar J <omarj819@gmail.com>
2024-01-23 22:23:40 +0200

    A

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-str1 = "Welcome to"
-str2 = "Holberton School!"
-print(f"{str1} {str2}")
+str1 = "Welcome"
+str2 = "to Holberton School!"
+print(str1 + " " + str2)
--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for char in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(char if char % 2 == 0 else char - 32), end="")
+    print("{:c}".format(char if char % 2 == 1 else char - 32), end="")
 
 print()
