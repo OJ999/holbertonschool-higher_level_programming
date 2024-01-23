6b6eea5ce8c6c2fb15283922af82e92489d6acd2
Omar J <omarj819@gmail.com>
2024-01-23 22:03:46 +0200

    A

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif number % 3 == 0:
+            print("Fizz", end=" ")
+        elif number % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(number, end=" ")
+
+if __name__ == "__main__":
+    fizzbuzz()
+    print("")
