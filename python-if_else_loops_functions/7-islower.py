7407d6e40522408659b7593f390c1e5d1ab22eb7
Omar J <omarj819@gmail.com>
2024-01-23 18:17:51 +0200

    A

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
-
 if __name__ == "__main__":
     print("a is {}".format("lower" if islower("a") else "upper"))
     print("H is {}".format("lower" if islower("H") else "upper"))
