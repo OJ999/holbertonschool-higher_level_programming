d49718e3f80950818d7873544f83496533eb0146
Omar J <omarj819@gmail.com>
2024-01-23 22:42:23 +0200

    A

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented"):str.find("object-oriented") + len("object-oriented programming with Python")])
+str1 = "object-oriented"
+str2 = " programming with"
+str3 = " Python"
+print(str1 + str2 + str3)
