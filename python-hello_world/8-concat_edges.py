89b8856daa3f6049da24819d45aa56fe5972179f
Omar J <omarj819@gmail.com>
2024-01-23 22:43:39 +0200

    A

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = " programming with"
-str3 = " Python"
-print(str1 + str2 + str3)
+print("".join(["object-oriented", " programming with", " Python"]))
