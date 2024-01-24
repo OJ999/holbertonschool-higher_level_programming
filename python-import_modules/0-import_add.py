6329927e088828315a097c73dd4409b604434cfa
Omar J <omarj819@gmail.com>
2024-01-24 19:23:09 +0200

    A

--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-from os import system
-
 if __name__ == "__main__":
-    system("./0-add.py")
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
