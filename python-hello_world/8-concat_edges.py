862679660030f18899aa8999c3268f7728537853
Omar J <omarj819@gmail.com>
2024-01-24 20:06:13 +0200

    A

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[107:123]}{'{0}'}{str[0:6]}{'{1}'}{str[66:106]}{'{2}'}\n")
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
