eac92e15986376a291333d74ff5abbaa001a18f1
Omar J <omarj819@gmail.com>
2024-01-24 19:33:42 +0200

    A

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+from add_0 import add
+
 a = 1
 b = 2
-add_0 = __import__('add_0').add
-print("{:d} + {:d} = {:d}".format(a, b, add_0(a, b)))
+
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))
