36e8af4f8672350ca370f56666ec8b66f7fa2b2b
Omar J <omarj819@gmail.com>
2024-01-30 17:34:43 +0200

    A

--- python-more_data_structures/11-mutiply_list_map.py
+++ python-more_data_structures/11-mutiply_list_map.py
@@ -3,16 +3,3 @@ def multiply_by_2(a_dictionary):
     for key, value in a_dictionary.items():
         new_dict[key] = value * 2
     return new_dict
-
-def print_sorted_dictionary(a_dictionary):
-    sorted_items = sorted(a_dictionary.items())
-    for key, value in sorted_items:
-        print(f"{key}: {value}")
-
-a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-new_dict = multiply_by_2(a_dictionary)
-print("Original dictionary:")
-print_sorted_dictionary(a_dictionary)
-print("--")
-print("Modified dictionary:")
-print_sorted_dictionary(new_dict)
