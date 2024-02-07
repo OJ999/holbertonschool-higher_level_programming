a2e31c10c252f3c176c0789e407a7c4795894e00
Omar J <omarj819@gmail.com>
2024-02-07 20:51:17 +0200

    A

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+"""Defines a class MyInt that inherits from int"""
+
+
 class MyInt(int):
-    """ Class that inherits from class int"""
+    """A class representing a rebel integer"""
 
     def __eq__(self, other):
-        """ Method that returns != check """
-        return int.__ne__(self, other)
+        """Overrides the equality operator"""
+        return super().__ne__(other)
 
     def __ne__(self, other):
-        """ Method that returns == check """
-        return int.__eq__(self, other)
-        
+        """Overrides the inequality operator"""
+        return super().__eq__(other)
+
+
+if __name__ == "__main__":
+    my_i = MyInt(3)
+    print(my_i)
+    print(my_i == 3)
+    print(my_i != 3)
