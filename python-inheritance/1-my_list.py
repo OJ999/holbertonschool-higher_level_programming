3783421d407c3ecac5bde74bdceec355bf245adb
Omar J <omarj819@gmail.com>
2024-02-07 20:34:43 +0200

    A

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+"""Defines a class MyList that inherits from list"""
+
+
 class MyList(list):
-    """ Class that inherits the attributes references of class list
-
-    Args:
-        list: class list
-
-    """
+    """A subclass of list"""
 
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
-        
+        """Prints the list in sorted order"""
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    my_list.print_sorted()
+    print(my_list)
