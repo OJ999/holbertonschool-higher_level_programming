743e22bbfc2263173351ed85e52fc0b5e78021f3
Omar J <omarj819@gmail.com>
2024-02-07 20:51:57 +0200

    A

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-def add_attribute(obj, name, value):
-    """ Function that adds a new attribute to an object
+"""Defines a function to add a new attribute to an object if possible"""
 
-    Args:
-        obj: object
-        name: attribute name
-        value: attribute value
 
-    Raises:
-        TypeError: when the attribute can't be added
-
-    """
-
+def add_attribute(obj, attribute, value):
+    """Adds a new attribute to an object if possible"""
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
-    setattr(obj, name, value)
-    
+    setattr(obj, attribute, value)
+
+
+if __name__ == "__main__":
+    class MyClass:
+        pass
+
+    mc = MyClass()
+    add_attribute(mc, "name", "John")
+    print(mc.name)
+
+    try:
+        a = "My String"
+        add_attribute(a, "name", "Bob")
+        print(a.name)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
