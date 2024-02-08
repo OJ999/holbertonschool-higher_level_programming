5a5b9e651711beb2dc165fd697cf9530698d781f
Omar J <omarj819@gmail.com>
2024-02-08 16:08:27 +0200

    A

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-""" Module that contains a function that reads from a file """
+"""Module to read a text file and print its content to stdout."""
 
 
 def read_file(filename=""):
-    """ Function that reads from a file
+    """Read a text file and print its content to stdout.
 
     Args:
-        filename: filename
-
-    Raises
-        Exception: when the file can be opened
+        filename (str): The name of the file to read.
 
     """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
 
-    with open(filename, 'r', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end='')
-        
+if __name__ == "__main__":
+    read_file()
