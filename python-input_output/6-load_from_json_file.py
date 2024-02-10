09343a7001300518c8a4105c216fc7547c55105f
Omar J <omarj819@gmail.com>
2024-02-10 16:57:59 +0200

    A

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Module to create an Object from a JSON file
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an Object from a JSON file
+
+    Args:
+        filename (str): The name of the JSON file to load from
+
+    Returns:
+        object: The Python data structure represented by the JSON file
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+
+if __name__ == "__main__":
+    filename = "my_list.json"
+    my_list = load_from_json_file(filename)
+    print(my_list)
+    print(type(my_list))
+
+    filename = "my_dict.json"
+    my_dict = load_from_json_file(filename)
+    print(my_dict)
+    print(type(my_dict))
+
+    try:
+        filename = "my_set_doesnt_exist.json"
+        my_set = load_from_json_file(filename)
+        print(my_set)
+        print(type(my_set))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        filename = "my_fake.json"
+        my_fake = load_from_json_file(filename)
+        print(my_fake)
+        print(type(my_fake))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e)))
