f2655b04afd086dcf6ab1d1c845f6773acd7900e
Omar J <omarj819@gmail.com>
2024-02-10 17:20:15 +0200

    A

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,33 +1,15 @@
 #!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-from os.path import exists
-from json import dumps, loads
-from contextlib import suppress
 
-# Function to save data to a JSON file
-def save_to_json_file(my_obj, filename):
-    with open(filename, 'w') as f:
-        f.write(dumps(my_obj))
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-# Function to load data from a JSON file
-def load_from_json_file(filename):
-    with open(filename, 'r') as f:
-        return (loads(f.read()))
-
-# Check if the file exists, if not create it
-filename = 'add_item.json'
-if not exists(filename):
-    with open(filename, 'w'):
-        pass
-
-# Load existing data from the file
-try:
-    data = load_from_json_file(filename)
-except:
-    data = []
-
-# Add command-line arguments to the data
-data.extend(sys.argv[1:])
-
-# Save the updated data to the file
-save_to_json_file(data, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
