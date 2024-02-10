2fef6914968e32129547d4650e25cf0dfa4a0be8
Omar J <omarj819@gmail.com>
2024-02-10 17:13:45 +0200

    A

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,58 +1,52 @@
 #!/usr/bin/python3
-""" Module to print status code """
 import sys
 
+def print_metrics(total_size, status_counts):
+    """
+    Print the computed metrics.
 
-class Magic:
-    """ Class to generates instances with dict and size"""
-    def __init__(self):
-        """ Init method """
-        self.dic = {}
-        self.size = 0
+    Args:
+        total_size (int): Total file size.
+        status_counts (dict): Dictionary containing counts of each status code.
+    """
+    print("File size: {:d}".format(total_size))
+    for status_code in sorted(status_counts.keys()):
+        print("{:d}: {:d}".format(status_code, status_counts[status_code]))
 
-    def init_dic(self):
-        """ Initialize dict """
-        self.dic['200'] = 0
-        self.dic['301'] = 0
-        self.dic['400'] = 0
-        self.dic['401'] = 0
-        self.dic['403'] = 0
-        self.dic['404'] = 0
-        self.dic['405'] = 0
-        self.dic['500'] = 0
+def parse_line(line):
+    """
+    Parse a line of input and extract the status code and file size.
 
-    def add_status_code(self, status):
-        """ add repeated number to the status code """
-        if status in self.dic:
-            self.dic[status] += 1
+    Args:
+        line (str): Input line to parse.
 
-    def print_info(self, sig=0, frame=0):
-        """ print status code """
-        print("File size: {:d}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] is not 0:
-                print("{}: {:d}".format(key, self.dic[key]))
+    Returns:
+        tuple: A tuple containing the status code (int) and file size (int).
+    """
+    parts = line.strip().split()
+    status_code = int(parts[-2])
+    file_size = int(parts[-1])
+    return status_code, file_size
 
-
-if __name__ == "__main__":
-    magic = Magic()
-    magic.init_dic()
-    nlines = 0
+def main():
+    total_size = 0
+    status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    line_count = 0
 
     try:
         for line in sys.stdin:
-            if nlines % 10 == 0 and nlines is not 0:
-                magic.print_info()
+            status_code, file_size = parse_line(line)
+            total_size += file_size
+            if status_code in status_counts:
+                status_counts[status_code] += 1
 
-            try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-                magic.size += int(list_line[-1].strip("\n"))
-            except:
-                pass
-            nlines += 1
+            line_count += 1
+            if line_count % 10 == 0:
+                print_metrics(total_size, status_counts)
+
     except KeyboardInterrupt:
-        magic.print_info()
+        print_metrics(total_size, status_counts)
         raise
-    magic.print_info()
-    
+
+if __name__ == "__main__":
+    main()
