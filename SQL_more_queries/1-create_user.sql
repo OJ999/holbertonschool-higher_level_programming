acbacfcbd72df532ad1c9ad64032055d23b2a96a
Omar J <omarj819@gmail.com>
2024-03-18 18:31:46 +0200

    A

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the user on all databases
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
